d1bd3020ee9684a07955ee1fb60a59ae316a56e2
Ajmal <ajmalbinabdulaziz@gmail.com>
2024-10-01 19:33:36 +0530

    deploy error 2

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -2,15 +2,7 @@ import { client } from "@/sanity/lib/client"
 import Gallery from "./Gallery"
 
 
-interface getImagesProps {
-  map(arg0: (gallery: any, index: any) => import("react").JSX.Element): import("react").ReactNode | Iterable<import("react").ReactNode>
-  id: string
-  _createdAt: string
-  title: string
-  mainImage: string
-}
-
-export const getImages = async ()=>{
+const getImages = async ()=>{
   const query = `*[_type=="gallery"]{
     _id,
     _createdAt,
@@ -26,7 +18,7 @@ export const getImages = async ()=>{
 
 async function page() {
 
-  const data: getImagesProps = await getImages()
+  const data = await getImages()
   return (
     <div>
         <Gallery data={data} />
