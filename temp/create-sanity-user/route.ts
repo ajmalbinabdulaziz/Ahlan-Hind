0adbe9b58cb848d7f1fa33879c995561ffe0372e
apple <apple@apples-MacBook-Pro-3.local>
2024-10-18 11:47:08 +0530

    solving issues 6

--- temp/create-sanity-user/route.ts
+++ temp/create-sanity-user/route.ts
@@ -1,28 +1,28 @@
-import { NextApiRequest } from "next";
-import { client } from "../../sanity/lib/client"
-import { currentUser } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
+// import { NextApiRequest } from "next";
+// import { client } from "../../sanity/lib/client"
+// import { currentUser } from "@clerk/nextjs/server";
+// import { NextResponse } from "next/server";
 
 
-export const GET = async (req: NextApiRequest) => {
-    const user = await currentUser();
-    if (!user) {
-        return NextResponse.redirect("/sign-in");
-    }
+// export const GET = async (req: NextApiRequest) => {
+//     const user = await currentUser();
+//     if (!user) {
+//         return NextResponse.redirect("/sign-in");
+//     }
 
-    const {id, firstName, lastName, emailAddresses} = user;
+//     const {id, firstName, lastName, emailAddresses} = user;
 
-    await client.createIfNotExists({
-            _type: "user",
-            _id: id,
-            firstName,
-            lastName,
-            email: emailAddresses[0].emailAddress
-        })
+//     await client.createIfNotExists({
+//             _type: "user",
+//             _id: id,
+//             firstName,
+//             lastName,
+//             email: emailAddresses[0].emailAddress
+//         })
 
-        const url = req.url?.split("/create-sanity-user")[0] || "/"
-            return NextResponse.redirect(url);
+//         const url = req.url?.split("/create-sanity-user")[0] || "/"
+//             return NextResponse.redirect(url);
     
 
-}
+// }
 
