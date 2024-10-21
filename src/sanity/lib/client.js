89c6a96823dbe6ced7a285e9d184dbbdc2a38e9f
apple <apple@apples-MacBook-Pro-3.local>
2024-10-21 16:27:33 +0530

    solving, pasted apiVersion in client.js

--- src/sanity/lib/client.js
+++ src/sanity/lib/client.js
@@ -5,7 +5,7 @@ import { createClient } from 'next-sanity'
 const dataset = 'production'
 const projectId = 'c4p78oz0'
 const token = 'skkTt7eCJLSEHWmxR7QNmgKc4xCuCFMAho1ieZpZ0fzbtW8nj6JSlGSS7m3Go0RCrWDonac6qNnwwlGY6Ei30blZjfG3Q8tMmhVglRNp4QhCRatADEzsAffgFu18QvDD2vYrh682XI76Di1CCeHDSf6BWGPwcHSnkiSrXp6AI1zaFlOf2RoG'
-
+const apiVersion = '2024-09-25'
 
 export const client = createClient({
   projectId,
